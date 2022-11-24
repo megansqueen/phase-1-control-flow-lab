658b62d4dd2ec63dc4e7625966b2a91d4750a2c2
Megan <esler6724@gmail.com>
2022-11-23 21:23:36 -0800

    commit tree 4a0c79bf490566e0797f6d22bf691dca94d3e01e

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let greeting;
+  if (someValue <= 400) {
+    greeting = `This one is on me!`;
+  }
+  else if (someValue <= 2000) {
+      greeting = `That will be twenty bucks.`;
+    }
+    else 
+      if (someValue <= 2500) {
+        greeting = `I will gladly take your thirty bucks.`
+      }
+      else
+        if (someValue > 2500) {
+          greeting = `No can do.`
+        }
+  
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let greeting;
+  if (city === `NYC`) {
+    greeting = `Ok, sounds good.`;
+  }
+  else
+    if (city !== `NYC`) {
+      greeting = `No go.`;
+    }
+  return greeting;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case `generous`: 
+     tip = `Thank you so much.`;
+    break;
+    case `not as generous`: 
+     tip =`Thank you.`;
+    break;
+    default: tip = `Bye.`
+  }
+  return tip;
 }
